103210e705465e031a8c5980a099970d7ec5165b
Lucas D'Alesio <dalesil@rose-hulman.edu>
2019-01-21 12:55:44 -0500

    m3 done

--- src/m3_nested_loops_in_printing.py
+++ src/m3_nested_loops_in_printing.py
@@ -264,7 +264,7 @@ def triangle_same_number_in_each_row(r):
     Precondition:  r is a non-negative integer.
     """
     # ------------------------------------------------------------------
-    # TODO: 6. Implement and test this function.
+    # DONE: 6. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     # IMPLEMENTATION RESTRICTION:
@@ -273,9 +273,9 @@ def triangle_same_number_in_each_row(r):
     #   would defeat the goal of providing practice at loops within loops.
     # ------------------------------------------------------------------
 
-    for j in range(r):
-        for k in range(1, r+1):
-            print(r, end='')
+    for j in range(1, r+1):
+        for k in range(j):
+            print(j, end='')
         print()
 
 def run_test_triangle_all_numbers_in_each_row():
@@ -311,7 +311,7 @@ def triangle_all_numbers_in_each_row(r):
     Precondition:  r is a non-negative integer.
     """
     # ------------------------------------------------------------------
-    # TODO: 7. Implement and test this function.
+    # DONE: 7. Implement and test this function.
     #          Some tests are already written for you (above).
     #
     # IMPLEMENTATION RESTRICTION:
@@ -320,6 +320,10 @@ def triangle_all_numbers_in_each_row(r):
     #   would defeat the goal of providing practice at loops within loops.
     # ------------------------------------------------------------------
 
+    for j in range(1, r + 1):
+        for k in range(j):
+            print(k + 1, end='')
+        print()
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
